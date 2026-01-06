e1d9b88fdbd8446a96fdf53ef215306ec71f6c3d
River_Moment <106030604+HateMate101@users.noreply.github.com>
2026-01-06 13:42:15 -0600

    better organization
    for projs

---
+++ Content/Projectiles/Enemies/Boss/ApexPlasmaBulletHostile.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Projectiles.Enemies.Boss
+    {
+    public class ApexPlasmaBulletHostile : ModProjectile
+        {
+        public override void SetStaticDefaults()
+            {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 8;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+            }
+        public override void SetDefaults()
+            {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.penetrate = 11;
+            Projectile.timeLeft = 1950;
+            Projectile.alpha = 100;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = true;
+            AIType = ProjectileID.BulletDeadeye;
+            }
+        public override void AI()
+            {
+            Lighting.AddLight(Projectile.Center, 0.55f, 0f, 0.55f);
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            }
+        public override bool PreDraw(ref Color lightColor)
+            {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+                {
+                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+                }
+            return true;
+            }
+        }
+    }
---
+++ Content/Projectiles/Items/ApexPlasmaBullet.cs
@@ -0,0 +1,68 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace HendecamMod.Content.Projectiles.Items
+{
+    public class ApexPlasmaBullet : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 8;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = 11;
+            Projectile.timeLeft = 195;
+            Projectile.alpha = 100;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 3;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            AIType = ProjectileID.Bullet;
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Projectile.damage = (int)(Projectile.damage * 0.95f);
+        }
+        public override void AI()
+        {
+            Lighting.AddLight(Projectile.Center, 0.55f, 0f, 0.55f);
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            return true;
+        }
+
+
+       
+
+    }
+
+}
