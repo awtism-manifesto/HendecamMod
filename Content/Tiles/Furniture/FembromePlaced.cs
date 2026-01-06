75a43f137a3afaed77196a5c5b0db6ed58ef5dd9
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 15:13:59 -0500

    Add files via upload

---
+++ Content/Tiles/Furniture/FembromePlaced.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace HendecamMod.Content.Tiles.Paintings
+{
+    // Simple 3x3 tile that can be placed on a wall
+    public class FembromePlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(175, 103, 54), Language.GetText("Custom Painting"));
+            DustType = 91;
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/TwinPosterPlaced.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace HendecamMod.Content.Tiles.Paintings
+{
+    // Simple 3x3 tile that can be placed on a wall
+    public class TwinPosterPlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.CoordinateHeights = new[]
+            {
+                16,
+                16,
+                16
+            };
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(175, 103, 54), Language.GetText("Custom Painting"));
+            DustType = 91;
+        }
+    }
+}
