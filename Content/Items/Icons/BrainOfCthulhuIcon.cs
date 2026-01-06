b3855a637f37fb3f3960e6f5cb9b4e92212c6f08
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 15:07:46 -0500

    Add files via upload

---
+++ Content/Items/Icons/BrainOfCthulhuIcon.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using HendecamMod.Content.Rarities;
+
+namespace HendecamMod.Content.Items.Icons
+{
+    public class BrainOfCthulhuIcon : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 3;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.rare = ModContent.RarityType<DarkGreen>();
+            Item.value = 10;
+            Item.maxStack = 9999;
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "A token for those who have slain the Brain of Cthulhu");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+
+            // Another method of hiding can be done if you want to hide just one line.
+            // tooltips.FirstOrDefault(x => x.Mod == "ExampleMod" && x.Name == "Verbose:RemoveMe")?.Hide();
+        }
+    }
+}
