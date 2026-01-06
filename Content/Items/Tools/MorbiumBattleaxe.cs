17174c10c5516115db4fe10ae7eb7345c94dfb02
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 15:02:54 -0500

    Add files via upload

---
+++ Content/Items/Tools/MorbiumBattleaxe.cs
@@ -0,0 +1,52 @@
+﻿using HendecamMod.Content.Items.Placeables;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Items.Tools
+{
+    public class MorbiumBattleaxe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 76;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 7;
+            Item.useAnimation = 18;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 6;
+            Item.value = 10000;
+            Item.rare = ItemRarityID.Yellow;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true; // Automatically re-swing/re-use this item after its swinging animation is over.
+
+            Item.axe = 25; // How much axe power the weapon has, note that the axe power displayed in-game is this value multiplied by 5
+            Item.attackSpeedOnlyAffectsWeaponAnimation = true; // Melee speed affects how fast the tool swings for damage purposes, but not how fast it can dig
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.NextBool(10))
+            { // This creates a 1/10 chance that a dust will spawn every frame that this item is in its 'Swinging' animation.
+              // Creates a dust at the hitbox rectangle, following the rules of our 'if' conditional.
+            }
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<MorbiumBar>(18);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+
+
+
+
+
+        }
+    }
+}
---
+++ Content/Items/Tools/MorbiumPickaxe.cs
@@ -0,0 +1,86 @@
+﻿using HendecamMod.Content.Items.Placeables;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Items.Tools
+{
+
+    public class MorbiumPickaxe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 36;
+            Item.height = 36;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 7;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 34;
+            Item.knockBack = 6;
+            Item.ChangePlayerDirectionOnShoot = false;
+            Item.pick = 205;
+            Item.tileBoost = 2;
+
+
+            Item.value = Item.buyPrice(gold: 5);
+            Item.rare = ItemRarityID.Yellow;
+            Item.UseSound = SoundID.Item1;
+
+
+
+            // If you want melee speed to only affect the swing speed of the weapon and not the shoot speed (not recommended)
+            // Item.attackSpeedOnlyAffectsWeaponAnimation = true;
+
+            // Normally shooting a projectile makes the player face the projectile, but if you don't want that (like the beam sword) use this line of code
+            // Item.ChangePlayerDirectionOnShoot = false;
+        }
+
+
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+
+            // Another method of hiding can be done if you want to hide just one line.
+            // tooltips.FirstOrDefault(x => x.Mod == "ExampleMod" && x.Name == "Verbose:RemoveMe")?.Hide();
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<MorbiumBar>(18);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+        }
+
+    }
+}
