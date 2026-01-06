4795ce4bd1a497065590a2055740aca12b7a2683
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 14:35:17 -0500

    Add files via upload

---
+++ Content/Buffs/MorbinTime.cs
@@ -0,0 +1,34 @@
+﻿using HendecamMod.Content.Dusts;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Buffs
+{
+    public class MorbinTime : ModBuff
+    {
+        public static readonly int AttackSpeedBonus = 11;
+        public static readonly int CritBonus = 11;
+        public static readonly int AdditiveDamageBonus = 11;
+
+
+
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+           
+            if (Main.rand.NextBool(2))
+            {
+                int dust = Dust.NewDust(player.position, player.width, player.height, ModContent.DustType<MorbiumDust>(),
+                    player.velocity.X * Main.rand.NextFloat(-1.99f, 1.99f), player.velocity.Y * Main.rand.NextFloat (-1.99f, 1.99f), 70, default, 3f);
+                Main.dust[dust].noGravity = true;
+            }
+            player.GetCritChance(DamageClass.Generic) += CritBonus;
+            player.GetAttackSpeed(DamageClass.Generic) += AttackSpeedBonus / 113f;
+            player.GetDamage(DamageClass.Generic) += AdditiveDamageBonus / 113f;
+            player.statDefense += 11;
+        }
+    }
+}
