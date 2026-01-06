72f768709004ec54fb415abb6da9abde31e1acd3
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 15:01:33 -0500

    Add files via upload

---
+++ Content/Items/Consumables/LeadskinPotion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Items.Consumables
+{
+    public class LeadskinPotion : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 20;
+
+            // Dust that will appear in these colors when the item with ItemUseStyleID.DrinkLiquid is used
+            ItemID.Sets.DrinkParticleColors[Type] = new Color[3] {
+                new Color(240, 240, 240),
+                new Color(200, 200, 200),
+                new Color(140, 140, 140)
+            };
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 26;
+            Item.useStyle = ItemUseStyleID.DrinkLiquid;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.useTurn = true;
+            Item.UseSound = SoundID.Item3;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(copper: 42);
+            Item.buffType = ModContent.BuffType<Buffs.LeadPoisoningDebuff>(); // Specify an existing buff to be applied when used.
+            Item.buffTime = 25200; // The amount of time the buff declared in Item.buffType will last in ticks. 5400 / 60 is 90, so this buff will last 90 seconds.
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.BottledWater, 1);
+            recipe.AddIngredient(ItemID.LeadBar, 1);
+            recipe.AddIngredient(ItemID.Daybloom, 1);
+            recipe.Register();
+            recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.BottledWater, 1);
+            recipe.AddIngredient(ItemID.IronBar, 1);
+            recipe.AddIngredient(ItemID.Daybloom, 1);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Items/Consumables/SpiritRum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Items.Consumables
+{
+    public class SpiritRum : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 20;
+
+            // Dust that will appear in these colors when the item with ItemUseStyleID.DrinkLiquid is used
+            ItemID.Sets.DrinkParticleColors[Type] = new Color[3] {
+                new Color(240, 240, 240),
+                new Color(200, 200, 200),
+                new Color(140, 140, 140)
+            };
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 26;
+            Item.useStyle = ItemUseStyleID.DrinkLiquid;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.useTurn = true;
+            Item.UseSound = SoundID.Item3;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.buffType = BuffID.NoBuilding;
+            Item.buffTime = 69420;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Ale, 1);
+            recipe.AddIngredient(ItemID.DirtBlock, 1);
+            recipe.AddTile(TileID.Bottles);
+            recipe.Register();
+            if (ModLoader.TryGetMod("ThoriumMod", out Mod MagThorium) && MagThorium.TryFind("SpiritDroplet", out ModItem SpiritDroplet))
+            {
+                recipe = CreateRecipe(10);
+                recipe.AddIngredient(ItemID.Ale, 10);
+                recipe.AddIngredient(SpiritDroplet.Type);
+                recipe.AddTile(TileID.Bottles);
+                recipe.Register();
+            }
+        }
+    }
+}
