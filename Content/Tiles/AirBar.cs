866afd0186475ee5778c57623851379a53dd0a11
River_Moment <106030604+HateMate101@users.noreply.github.com>
2026-01-06 13:41:14 -0600

    yippie
    :3

---
+++ Content/Tiles/AirBar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace HendecamMod.Content.Tiles
+{
+    public class AirBar : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CoordinateHeights = [16];
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleMultiplier = 27;
+            TileObjectData.newTile.StyleWrapLimit = 27;
+            TileObjectData.newTile.UsesCustomCanPlace = false;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            VanillaFallbackOnModDeletion = TileID.MetalBars;
+
+            AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.MetalBar"));
+        }
+    }
+}
---
+++ Content/Tiles/StoneBar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace HendecamMod.Content.Tiles
+{
+    public class StoneBar : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            VanillaFallbackOnModDeletion = TileID.MetalBars;
+
+            AddMapEntry(new Color(200, 200, 200), Language.GetText("MapObject.MetalBar"));
+        }
+
+        public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+        {
+            if (!WorldGen.SolidTileAllowBottomSlope(i, j + 1))
+            {
+                WorldGen.KillTile(i, j);
+            }
+            return true;
+        }
+    }
+}
