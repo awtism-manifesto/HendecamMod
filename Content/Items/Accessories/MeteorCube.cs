886290babf397d68a29a810382874ff90cf7b83c
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 14:59:35 -0500

    Add files via upload

---
+++ Content/Items/Accessories/MeteorCube.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using HendecamMod.Content.Items.Materials;
+
+namespace HendecamMod.Content.Items.Equippable
+{
+    public class MeteorCube : ModItem
+    {
+
+
+        public override void SetDefaults()
+        {
+            // Modders can use Item.DefaultToRangedWeapon to quickly set many common properties, such as: useTime, useAnimation, useStyle, autoReuse, DamageType, shoot, shootSpeed, useAmmo, and noMelee. These are all shown individually here for teaching purposes.
+
+            // Common Properties
+            Item.width = 26; // Hitbox width of the item.
+            Item.height = 26; // Hitbox height of the item.
+            Item.rare = ItemRarityID.Green; // The color that the item's name will be in-game.
+            Item.value = 1000;
+            Item.maxStack = 1;
+            Item.accessory = true;
+            Item.defense = 5;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.maxFallSpeed = player.maxFallSpeed * 4f;
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "Quadrouples your max fall speed");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "Good thing this can't become a black hole")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+
+            // Another method of hiding can be done if you want to hide just one line.
+            // tooltips.FirstOrDefault(x => x.Mod == "ExampleMod" && x.Name == "Verbose:RemoveMe")?.Hide();
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<CubicMold>(1);
+            recipe.AddIngredient(ItemID.MeteoriteBar, 12);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Items/Accessories/ZincCube.cs
@@ -0,0 +1,75 @@
+﻿using HendecamMod.Content.Rarities;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using HendecamMod.Content.Items.Materials;
+
+namespace HendecamMod.Content.Items.Equippable
+{
+    public class ZincCube : ModItem
+    {
+
+
+        public override void SetDefaults()
+        {
+            // Modders can use Item.DefaultToRangedWeapon to quickly set many common properties, such as: useTime, useAnimation, useStyle, autoReuse, DamageType, shoot, shootSpeed, useAmmo, and noMelee. These are all shown individually here for teaching purposes.
+
+            // Common Properties
+            Item.width = 26; // Hitbox width of the item.
+            Item.height = 26; // Hitbox height of the item.
+            Item.rare = ModContent.RarityType<BurntOrange>();
+            Item.value = 500;
+            Item.maxStack = 1;
+            Item.accessory = true;
+            Item.defense = 3;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.maxFallSpeed = player.maxFallSpeed * 2f;
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "Avalon Compatability Item");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "Doubles your fall speed")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+
+            // Another method of hiding can be done if you want to hide just one line.
+            // tooltips.FirstOrDefault(x => x.Mod == "ExampleMod" && x.Name == "Verbose:RemoveMe")?.Hide();
+        }
+        public override void AddRecipes()
+        {
+
+            if (ModLoader.TryGetMod("Avalon", out Mod Avalon) && Avalon.TryFind<ModItem>("ZincBar", out ModItem ZincBar))
+            {
+                Recipe recipe = CreateRecipe();
+                recipe.AddIngredient<CubicMold>(1);
+                recipe.AddIngredient(ZincBar.Type, 12);
+                recipe.AddTile(TileID.Anvils);
+                recipe.Register();
+            }
+            else { }
+        }
+    }
+}
