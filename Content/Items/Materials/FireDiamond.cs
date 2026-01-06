ff28de49d3c86a4e14d91da283e72903065b349b
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-06 15:09:44 -0500

    Add files via upload

---
+++ Content/Items/Materials/FireDiamond.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace HendecamMod.Content.Items.Materials
+{
+    public class FireDiamond : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 25;
+        }
+        public override void SetDefaults()
+        {
+            // Modders can use Item.DefaultToRangedWeapon to quickly set many common properties, such as: useTime, useAnimation, useStyle, autoReuse, DamageType, shoot, shootSpeed, useAmmo, and noMelee. These are all shown individually here for teaching purposes.
+
+            // Common Properties
+            Item.width = 32; // Hitbox width of the item.
+            Item.height = 32; // Hitbox height of the item.
+            Item.rare = ItemRarityID.Blue; // The color that the item's name will be in-game.
+            Item.value = 500;
+            Item.maxStack = 9999;
+            Item.alpha = 50;
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "An old currency used by demons");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+
+            // Another method of hiding can be done if you want to hide just one line.
+            // tooltips.FirstOrDefault(x => x.Mod == "ExampleMod" && x.Name == "Verbose:RemoveMe")?.Hide()
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe(2);
+            recipe.AddIngredient(ItemID.Diamond, 1);
+            recipe.AddIngredient(ItemID.LivingFireBlock, 1);
+            recipe.Register();
+            recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Diamond, 1);
+            recipe.AddIngredient(ItemID.Hellstone, 1);
+            recipe.Register();
+
+        }
+    }
+}
