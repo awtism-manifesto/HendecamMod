ae305ed85887d9ad7ee027c459b86152c729dd4e
autism_manifesto <awpower005@gmail.com>
2026-01-09 15:40:03 -0500

    Adding razor rotors

---
+++ Content/Items/RazorRotors.cs
@@ -0,0 +1,97 @@
+﻿using HendecamMod.Content.Projectiles;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace HendecamMod.Content.Items
+{
+    public class RazorRotors : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+          
+            ItemID.Sets.ItemsThatAllowRepeatedRightClick[Type] = true;
+        }
+
+
+        public override void SetDefaults()
+        {
+            // Common Properties
+            Item.rare = ItemRarityID.Cyan;
+            Item.value = 350000; // The number and type of coins item can be sold for to an NPC
+
+            // Use Properties
+            Item.useStyle = ItemUseStyleID.Swing; // How you use the item (swinging, holding out, etc.)
+            Item.useAnimation = 30; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+            Item.useTime = 30; // The length of the item's use time in ticks (60 ticks == 1 second.)
+            Item.UseSound = SoundID.Item71; // The sound that this item plays when used.
+            Item.autoReuse = true; // Allows the player to hold click to automatically use the item again. Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
+
+            // Weapon Properties
+            Item.damage = 115;
+            Item.knockBack = 7.5f;
+            Item.noUseGraphic = true; // When true, the item's sprite will not be visible while the item is in use. This is true because the spear projectile is what's shown so we do not want to show the spear sprite as well.
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+            Item.noMelee = true; // Allows the item's animation to do damage. This is important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+            Item.ArmorPenetration = 25;
+            // Projectile Properties
+            Item.shootSpeed = 0f; // The speed of the projectile measured in pixels per frame.
+            Item.shoot = ModContent.ProjectileType<RazorRotorsProj>(); // The projectile that is fired from this weapon
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+
+
+            return true;
+
+
+        }
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                Item.useStyle = ItemUseStyleID.Shoot;
+                Item.shoot = ModContent.ProjectileType<RazorRotorThrown>();
+                Item.shootSpeed = 28.5f; 
+            }
+            else
+            {
+                Item.useStyle = ItemUseStyleID.Swing;
+              
+                Item.shoot = ModContent.ProjectileType<RazorRotorsProj>();
+                Item.shootSpeed = 0f; 
+            }
+
+            return base.CanUseItem(player);
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "Rapidly spins around, damaging everything around you");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "Right click to throw the smaller, stabilizer rotor")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "Ignores 25 enemy defense")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            
+        }
+       
+       
+       
+    }
+}
---
+++ Content/Projectiles/RazorRotorThrown.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HendecamMod.Content.Projectiles
+{
+    // This example is similar to the Wooden Arrow projectile
+    public class RazorRotorThrown : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+           
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 104; // The width of projectile hitbox
+            Projectile.height = 104; // The height of projectile hitbox
+            Projectile.tileCollide = false;
+            Projectile.arrow = false;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
+            Projectile.timeLeft = 74;
+            Projectile.penetrate = 10;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 4;
+        }
+
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+
+            for (int i = 0; i < 8; i++) 
+            {
+                Dust dust = Dust.NewDustDirect(target.position, target.width, target.height, DustID.Blood);
+                dust.noGravity = true;
+                dust.velocity *= 6.5f;
+                dust.scale *= 1.5f;
+            }
+
+
+
+
+        }
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            if (Projectile.timeLeft <= 37)
+            {
+                float length = Projectile.velocity.Length();
+                float targetAngle = Projectile.AngleTo(player.Center);
+                Vector2 position0 = Projectile.position;
+                Vector2 targetPosition0 = player.Center;
+                Vector2 direction = targetPosition0 - position0;
+                Projectile.velocity = Projectile.velocity.ToRotation().AngleTowards(targetAngle, MathHelper.ToRadians(360f)).ToRotationVector2() * length;
+            }
+            if (Projectile.timeLeft <= 13)
+            {
+               
+                    Projectile.velocity *= 1.07f;
+
+                
+            }
+
+            
+
+            Projectile.rotation += 0.425f;
+           
+        }
+
+        
+    }
+}
