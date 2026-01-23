72b07a0388e69af556b48649ed2c35a9da19eb8d
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-23 07:05:44 -0500

    Oasis Grass

---
+++ Content/Items/Placeables/OasisGrass.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using HendecamMod.Content.Tiles.Blocks;
+
+namespace HendecamMod.Content.Items.Placeables
+{
+    public class OasisGrass : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<OasisGrassPlaced>());
+            Item.width = 12;
+            Item.height = 12;
+
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.GrassSeeds, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.AddCondition(Condition.NearWater);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Tiles/Blocks/OasisGrassPlaced.cs
@@ -0,0 +1,40 @@
+﻿using HendecamMod.Content.Dusts;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Threading;
+using Terraria;
+using Terraria.Chat;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace HendecamMod.Content.Tiles.Blocks
+{
+    public class OasisGrassPlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileMerge[TileID.ClayBlock][Type] = true;
+            Main.tileMerge[TileID.Mud][Type] = true;
+            Main.tileMerge[TileID.HardenedSand][Type] = true;
+            Main.tileMerge[TileID.SnowBlock][Type] = true;
+            Main.tileMerge[TileID.Sand][Type] = true;
+            Main.tileMerge[TileID.Sandstone][Type] = true;
+            Main.tileMerge[TileID.Ebonsand][Type] = true;
+            Main.tileMerge[TileID.Crimsand][Type] = true;
+            Main.tileMerge[TileID.Pearlsand][Type] = true;
+            Main.tileMerge[TileID.Grass][Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            HitSound = SoundID.Dig;
+
+            AddMapEntry(new Color(170, 142, 69));
+
+        }
+    }
+}
