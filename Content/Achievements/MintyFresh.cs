fd2f6a2084679cf4a8b84969a1b271f2db8c8c7c
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-25 10:36:37 -0500

    achievements

---
+++ Content/Achievements/MintyFresh.cs
@@ -0,0 +1,22 @@
+﻿using HendecamMod.Content.Items;
+using Terraria;
+using Terraria.Achievements;
+using Terraria.GameContent.Achievements;
+using Terraria.ID;
+using Terraria.ModLoader;
+using HendecamMod.Content.Tiles;
+using HendecamMod.Content.Tiles.Blocks;
+
+namespace HendecamMod.Content.Achievements;
+
+public class MintyFresh : ModAchievement
+{
+    public TileDestroyedCondition Condition { get; private set; }
+
+    public override void SetStaticDefaults()
+    {
+        Achievement.SetCategory(AchievementCategory.Explorer);
+        Condition = AddTileDestroyedCondition([ModContent.TileType<MintalOrePlaced>()]);
+    }
+
+}
---
+++ Content/Achievements/SandyShores.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.Achievements;
+using Terraria.GameContent.Achievements;
+using Terraria.ModLoader;
+using HendecamMod.Content.Items;
+using HendecamMod.Content.Items.Placeables;
+
+namespace HendecamMod.Content.Achievements;
+
+public class SandyShores : ModAchievement
+{
+    public ItemCraftCondition Condition { get; private set; }
+
+    public override void SetStaticDefaults()
+    {
+        Achievement.SetCategory(AchievementCategory.Collector);
+        Condition = AddItemCraftCondition(ModContent.ItemType<HurricaneGun>());
+    }
+
+}
