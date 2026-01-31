1673133cc220ce5dc26ca68e7eebb8a40e4e9abc
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-01-31 09:52:14 -0500

    icon additions part one

---
+++ Content/Items/Icons/IceQueenIcon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using HendecamMod.Content.Rarities;
+
+namespace HendecamMod.Content.Items.Icons;
+
+public class IceQueenIcon : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 3;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 32;
+        Item.height = 32;
+        Item.rare = ModContent.RarityType<DarkGreen>();
+        Item.value = 750000;
+        Item.maxStack = 9999;
+    }
+
+    public override void ModifyTooltips(List<TooltipLine> tooltips)
+    {
+        var line = new TooltipLine(Mod, "Face", "A token for those who have slain the Ice Queen");
+        tooltips.Add(line);
+
+        line = new TooltipLine(Mod, "Face", "")
+        {
+            OverrideColor = new Color(255, 255, 255)
+        };
+        tooltips.Add(line);
+
+        foreach (var l in tooltips)
+        {
+            if (l.Name.EndsWith(":RemoveMe"))
+            {
+                l.Hide();
+            }
+        }
+    }
+}
