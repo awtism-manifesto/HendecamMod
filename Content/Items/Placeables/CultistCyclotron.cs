31126723535a4a91e807ad0b31bb2a721eb73afc
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-02-03 20:11:46 -0500

    cultist cyclotron

---
+++ Content/Items/Placeables/CultistCyclotron.cs
@@ -0,0 +1,52 @@
+﻿using HendecamMod.Content.Items.Materials;
+using System.Collections.Generic;
+
+namespace HendecamMod.Content.Items.Placeables;
+
+public class CultistCyclotron : ModItem
+{
+    public override void SetDefaults()
+    {
+        // Common Properties
+        Item.width = 20; // Hitbox width of the item.
+        Item.height = 20; // Hitbox height of the item.
+        Item.rare = ItemRarityID.White; // The color that the item's name will be in-game.
+        Item.value = 250000;
+        Item.maxStack = 9999;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.useAnimation = 15;
+        Item.useTime = 15;
+        Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.CultistCyclotronPlaced>());
+    }
+
+    public override void ModifyTooltips(List<TooltipLine> tooltips)
+    {
+        // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+        var line = new TooltipLine(Mod, "Face", "");
+        tooltips.Add(line);
+
+        line = new TooltipLine(Mod, "Face", "")
+        {
+            OverrideColor = new Color(255, 255, 255)
+        };
+        tooltips.Add(line);
+
+        // Here we will hide all tooltips whose title end with ':RemoveMe'
+        // One like that is added at the start of this method
+        foreach (var l in tooltips)
+        {
+            if (l.Name.EndsWith(":RemoveMe"))
+            {
+                l.Hide();
+            }
+        }
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient<WoodenStick>(69420);
+        recipe.AddTile(TileID.WorkBenches);
+        recipe.Register();
+    }
+}
---
+++ Content/Tiles/Furniture/CultistCyclotronPlaced.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.Localization;
+using Terraria.ObjectData;
+
+namespace HendecamMod.Content.Tiles.Furniture;
+
+public class CultistCyclotronPlaced : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        // Properties
+        Main.tileSolidTop[Type] = false;
+        Main.tileNoAttach[Type] = true;
+        Main.tileFrameImportant[Type] = true;
+        TileID.Sets.DisableSmartCursor[Type] = true;
+        TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
+
+        DustType = DustID.Stone;
+        AdjTiles = new int[] { TileID.LunarCraftingStation };
+        TileObjectData.newTile.Width = 4;
+        TileObjectData.newTile.Height = 4;
+        TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16 };
+        TileObjectData.addTile(Type);
+        // Etc
+        AddMapEntry(new Color(64, 64, 64), Language.GetText(""));
+    }
+
+    public override void NumDust(int x, int y, bool fail, ref int num)
+    {
+        num = fail ? 1 : 3;
+    }
+}
