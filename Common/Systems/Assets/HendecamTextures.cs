ae39b921069c2750ce12ec0bf1361281a2d591c6
”Lemon <bonixsrbija@gmail.com>
2026-04-11 16:34:06 +0200

    Added true magic pixel texture
    Just a pixle useful for drawing

--- Common/Systems/Assets/HendecamTextures.cs
+++ Common/Systems/Assets/HendecamTextures.cs
@@ -5,11 +5,14 @@ namespace HendecamMod.Common.Systems.Assets;
 
 public class HendecamTextures : ModSystem
 {
-    public static string GlowBallTexturePath { get; private set; } = "Hendecam/Assets/Textures/Misc/GlowBall";
+    public static string GlowBallTexturePath { get; private set; } = "HendecamMod/Assets/Textures/Misc/GlowBall";
+    public static string TrueMagicPixelPath { get; private set; } = "HendecamMod/Assets/Textures/Misc/TrueMagicPixel";
 
     public static Asset<Texture2D> GlowBallTexture { get; private set; }
+    public static Asset<Texture2D> TrueMagicPixel { get; private set; }
     public override void Load()
     {
         GlowBallTexture = Request<Texture2D>(GlowBallTexturePath);
+        TrueMagicPixel = Request<Texture2D>(TrueMagicPixelPath);
     }
 }
