2bcb6cb29ce9a8946bd886cacd3cc53e624f17c2
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-04-26 08:22:28 -0400

    recipe group stuff

---
+++ Common/Systems/RecipeGroupAdditions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Localization;
+
+namespace HendecamMod.Common.Systems
+{
+    public class RecipeGroupAdditions : ModSystem
+    {
+        public override void AddRecipeGroups()
+        {
+            RecipeGroup group = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.3")} {Lang.GetItemNameValue(ItemID.StoneBlock)}", ItemID.StoneBlock, ItemID.EbonstoneBlock, ItemID.PearlstoneBlock, ItemID.CrimstoneBlock);
+            RecipeGroup.RegisterGroup(nameof(ItemID.StoneBlock), group);
+        }
+    }
+}
---
+++ Common/Systems/RecipeGroupEdits.cs
@@ -0,0 +1,23 @@
+﻿using HendecamMod.Content.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HendecamMod.Common.Systems
+{
+    public class RecipeGroupEdits : ModSystem
+    {
+        public override void AddRecipeGroups()
+        {
+            if (RecipeGroup.recipeGroupIDs.ContainsKey("Wood"))
+            {
+                int index = RecipeGroup.recipeGroupIDs["Wood"];
+                RecipeGroup group = RecipeGroup.recipeGroups[index];
+                group.ValidItems.Add(ModContent.ItemType<PoorMahogany>());
+                group.ValidItems.Add(ModContent.ItemType<Pykrete>());
+            }
+        }
+    }
+}
