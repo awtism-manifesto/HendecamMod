162399665ed046a419242c995a7bcc614b2c6222
MagnoliaBreadMan <magnoliamax13@gmail.com>
2026-07-16 14:44:52 -0400

    Iron and Lead cubes

--- Content/Items/Accessories/Cubes/LeadCube.cs
+++ Content/Items/Accessories/Cubes/LeadCube.cs
@@ -3,27 +3,29 @@ using HendecamMod.Content.Items.Materials;
 
 namespace HendecamMod.Content.Items.Accessories.Cubes;
 
-public class SilverCube : ModItem
+public class LeadCube : ModItem
 {
     public override void SetDefaults()
     {
         Item.width = 26;
         Item.height = 26;
         Item.rare = ItemRarityID.Blue;
-        Item.value = 37000;
+        Item.value = 36000;
         Item.maxStack = 1;
         Item.accessory = true;
-        Item.defense = 3;
+        Item.defense = 10;
     }
 
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
-        player.maxFallSpeed = player.maxFallSpeed * 2f;
+        player.maxFallSpeed = player.maxFallSpeed * 0.5f;
+        player.accRunSpeed *= 0.5f;
+        player.moveSpeed *= 0.5f;
     }
 
     public override void ModifyTooltips(List<TooltipLine> tooltips)
     {
-        var line = new TooltipLine(Mod, "Face", "Doubles your fall speed");
+        var line = new TooltipLine(Mod, "Face", "Halves player movement speed");
         tooltips.Add(line);
 
         line = new TooltipLine(Mod, "Face", "")
@@ -45,7 +47,7 @@ public class SilverCube : ModItem
     {
         Recipe recipe = CreateRecipe();
         recipe.AddIngredient<CubicMold>();
-        recipe.AddIngredient(ItemID.SilverBar, 12);
+        recipe.AddIngredient(ItemID.LeadBar, 12);
         recipe.AddTile(TileID.Anvils);
         recipe.Register();
     }
